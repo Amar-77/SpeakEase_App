b1473157b7268fb8dcce57f08c141252bb5f2038
A K Amarjith <akamarjithajith@gmail.com>
2025-12-26 01:13:08 +0530

    new after adding env

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // Imports
 import 'screens/auth/login_screen.dart';
@@ -10,6 +11,8 @@ import 'screens/teacher/home/teacher_home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
